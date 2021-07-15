2dd3d21defc353898f94410b6f09822051f2eb97
Pedro Kazi <finitodias@gmail.com>
2021-07-15 16:55:58 -0300

    em processo
    adicionado livros em processo

--- ld5/java/script.js
+++ ld5/java/script.js
@@ -34,16 +34,23 @@ function subToggle() {
     var nav = document.getElementById('subnav')
     var arrow = document.getElementById('mais')
     var novo = document.getElementById('novo')
+    var arrowBook = document.getElementsByClassName('hide')
 
     if (window.screen.width > 600) {
         nav.classList.toggle('active')
         arrow.classList.toggle('up')
         arrow.classList.toggle('down')
+
     } else {
         nav.classList.toggle('active')
         arrow.classList.toggle('up')
         arrow.classList.toggle('down')
         novo.classList.toggle('padbot')
+    }
+    for (let i = 0; i < arrowBook.length; i++) {
+        if (arrowBook[i].style.display != "none") {
+            arrowBook[i].style.display = "none"
+        }
     }
 }
 
@@ -55,4 +62,20 @@ function bookToggle(book, arrow) {
     arrow.classList.toggle('up')
     arrow.classList.toggle('down')
     
+}
+
+function toggleTipos() {
+    var tipo = document.getElementById('tipos').value
+    var texto = document.getElementById('arquivos')
+    var button = document.getElementById('enviar')
+
+    if (tipo == 0) {
+        texto.innerHTML = "Selecione o tipo de livro que quer produzir"
+    } else if (tipo == 1){
+        texto.innerHTML = "Texto em .docx ou em .pdf <br> imagens com transparência em .png <br> imagens sem transparência em .jpg"
+    } else if (tipo == 2) {
+        texto.innerHTML = "Texto em .docx ou em .pdf <br> imagens com transparência em .png <br> imagens sem transparência em .jpg <br> audio em .mp3"
+    } else if (tipo == 3) {
+        texto.innerHTML = "imagens de cada página sem falas em .jpg (apenas balões de fala) <br> falas separadas em um .docx ou em um .pdf"
+    }
 }
