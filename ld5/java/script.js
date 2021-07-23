d10ec6abb72891c6e08d0fb5225ffc097bd581b5
Pedro Kazi <finitodias@gmail.com>
2021-07-23 18:47:38 -0300

    ld5 rework

--- ld5/java/script.js
+++ ld5/java/script.js
@@ -1,81 +1,69 @@
-function toggle() {
-    var type = document.getElementById('pessoa')
-    var form1 = document.getElementById('escritor')
-    var form2 = document.getElementById('editora')
+function loginFormShow() {
+    var select = document.getElementById("login-choice");
+    select.classList.toggle('active');
+}
 
-    if (type.value == 0) {
-        form2.style.maxHeight = "0px"
-        form1.style.maxHeight = "0px"
-    } else if (type.value == 1) {
-        form2.style.maxHeight = "0px"
-        form1.style.maxHeight = "100%"
+function cadFormShow() {
+    var select = document.getElementById("cadastro-choice");
+    select.classList.toggle('active')
+}
+
+function typeSelectLogin(value) {
+    var cpf = document.getElementById("login-cpf-form")
+    var cnpj = document.getElementById("login-cnpj-form")
+
+    if (value == 0) {
+        cpf.classList.remove('active')
+        cnpj.classList.remove('active')
+    } else if (value == 1) {
+        cpf.classList.add('active')
+        cnpj.classList.remove('active')
     } else {
-        form1.style.maxHeight = "0px"
-        form2.style.maxHeight = "100%"
+        cpf.classList.remove('active')
+        cnpj.classList.add('active')
     }
 }
+function typeSelectCad(value) {
+    var cpf = document.getElementById("cadastro-cpf-form")
+    var cnpj = document.getElementById("cadastro-cnpj-form")
 
-function navToggle() {
-    var nav = document.getElementById('menu')
-    var arrowBook = document.getElementsByClassName('hide')
-
-    nav.classList.toggle("active")
-
-    for (let i = 0; i < arrowBook.length; i++) {
-        if (arrowBook[i].style.display == "none") {
-            arrowBook[i].style.display = "inline-block"
-        } else {
-            arrowBook[i].style.display = "none"
-        }
-    }
-}
-
-function subToggle() {
-    var nav = document.getElementById('subnav')
-    var arrow = document.getElementById('mais')
-    var novo = document.getElementById('novo')
-    var arrowBook = document.getElementsByClassName('hide')
-
-    if (window.screen.width > 600) {
-        nav.classList.toggle('active')
-        arrow.classList.toggle('up')
-        arrow.classList.toggle('down')
-
+    if (value == 0) {
+        cpf.classList.remove('active')
+        cnpj.classList.remove('active')
+    } else if (value == 1) {
+        cpf.classList.add('active')
+        cnpj.classList.remove('active')
     } else {
-        nav.classList.toggle('active')
-        arrow.classList.toggle('up')
-        arrow.classList.toggle('down')
-        novo.classList.toggle('padbot')
+        cpf.classList.remove('active')
+        cnpj.classList.add('active')
     }
-    for (let i = 0; i < arrowBook.length; i++) {
-        if (arrowBook[i].style.display != "none") {
-            arrowBook[i].style.display = "none"
+}
+
+function setaVolta() {
+    var login = document.getElementById("login-choice");
+    var cadastro = document.getElementById("cadastro-choice");
+
+    login.classList.remove('active')
+    cadastro.classList.remove('active')
+}
+
+const section = document.querySelectorAll("section");
+
+const options = {
+    rootMargin: "-150px"
+}
+
+const observer = new IntersectionObserver(function(entries, observer){
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
         }
-    }
-}
+        if (entry.target.id == "example") {
+        }
+        observer.unobserve(entry.target)
+    })
+}, options);
 
-function bookToggle(book, arrow) {
-    var livro = document.getElementById(book)
-    var arrow = document.getElementById(arrow)
-
-    livro.classList.toggle('active')
-    arrow.classList.toggle('up')
-    arrow.classList.toggle('down')
-    
-}
-
-function toggleTipos() {
-    var tipo = document.getElementById('tipos').value
-    var texto = document.getElementById('arquivos')
-    var button = document.getElementById('enviar')
-
-    if (tipo == 0) {
-        texto.innerHTML = "Selecione o tipo de livro que quer produzir"
-    } else if (tipo == 1){
-        texto.innerHTML = "Texto em .docx ou em .pdf <br> imagens com transparência em .png <br> imagens sem transparência em .jpg"
-    } else if (tipo == 2) {
-        texto.innerHTML = "Texto em .docx ou em .pdf <br> imagens com transparência em .png <br> imagens sem transparência em .jpg <br> audio em .mp3"
-    } else if (tipo == 3) {
-        texto.innerHTML = "imagens de cada página sem falas em .jpg (apenas balões de fala) <br> falas separadas em um .docx ou em um .pdf"
-    }
-}
+section.forEach(section => {
+    observer.observe(section)
+})
